080a70769eb06754003e8f83ffbe51eb8ef3056f
Jeff Bazinet <139579570+jeff-bazinet@users.noreply.github.com>
2024-02-15 13:41:28 -0500

    Add playGame and helper functions. Use switch to handle the different combinations. Use forEach in choices array to match user input.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+# This project allows a user to play a game of rock, # paper, scissors against a computer.
--- script.js
+++ script.js
@@ -1,1 +1,83 @@
-console.log("test")
+const choices = ['Rock', 'Paper', 'Scissors'];
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3);
+}
+
+function getComputerChoice() {
+  return choices[getRandomNumber()];
+}
+
+function getPlayerSelection() {
+  let playerInput = prompt('Enter your choice - Rock, Paper or Scissors');
+  let playerChoice;
+
+  choices.forEach((choice) => {
+    if (choice.toLowerCase() === playerInput.toLowerCase()) {
+      playerChoice = choice;
+    }
+  });
+
+  if(!playerChoice){
+    console.log()
+  }
+  return playerChoice;
+}
+
+function printWinText(player, computer) {
+  console.log(
+    `Player: ${player} - Computer: ${computer} - You Win! ${player} Beats ${computer}!`
+  );
+}
+function printLossText(player, computer) {
+  console.log(
+    `Player: ${player} - Computer: ${computer} - You Lose! ${computer} Beats ${player}!`
+  );
+}
+function printTieText(player, computer) {
+  console.log(`Player: - ${player} Computer: ${computer} - You Tie!`);
+}
+
+function comparePlayers(player, computer) {
+  switch (player) {
+    case 'Rock':
+      if (computer === 'Scissors') {
+        printWinText(player, computer);
+        break;
+      } else if (computer === 'Paper') {
+        printLossText(player, computer);
+        break;
+      } else if (computer === 'Rock') {
+        printTieText(player, computer);
+        break;
+      }
+    case 'Paper':
+      if (computer === 'Rock') {
+        printWinText(player, computer);
+        break;
+      } else if (computer === 'Scissors') {
+        printLossText(player, computer);
+        break;
+      } else if (computer === 'Paper') {
+        printTieText(player, computer);
+        break;
+      }
+    case 'Scissors':
+      if (computer === 'Paper') {
+        printWinText(player, computer);
+        break;
+      } else if (computer === 'Rock') {
+        printLossText(player, computer);
+        break;
+      } else if (computer === 'Scissors') {
+        printTieText(player, computer);
+        break;
+      }
+  }
+}
+
+function playGame(player, computer) {
+  comparePlayers(player, computer);
+}
+
+playGame(getPlayerSelection(), getComputerChoice());
