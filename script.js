3d792bebfee54344a0ac35d1286de15bce7f8cfe
Jeff Bazinet <139579570+jeff-bazinet@users.noreply.github.com>
2024-02-18 22:20:49 -0500

    Added total score to print results and reset score once player or computer wins.

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const choices = ['Rock', 'Paper', 'Scissors'];
 let buttonRock = document.querySelector('.rock-hand');
 let buttonPaper = document.querySelector('.paper-hand');
 let buttonScissors = document.querySelector('.scissors-hand');
-let resultsDiv = document.querySelector('.results');
+let resultsText = document.querySelector('.result-text');
+let resultsScore = document.querySelector('.result-score');
+let playerScore = 0;
+let computerScore = 0;
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 3);
@@ -14,22 +17,27 @@ function getComputerChoice() {
 }
 
 function printWinText(player, computer) {
-  resultsDiv.textContent = `The computer chose ${computer}. You Win! ${player} Beats ${computer}!`;
+  resultsText.textContent = `The computer chose ${computer}. You Win! ${player} Beats ${computer}!`;
+  resultsScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
 }
 function printLossText(player, computer) {
-  resultsDiv.textContent = `The computer chose ${computer}. You Lose! ${computer} Beats ${player}!`;
+  resultsText.textContent = `The computer chose ${computer}. You Lose! ${computer} Beats ${player}!`;
+  resultsScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
 }
 function printTieText(player, computer) {
-  resultsDiv.textContent = `The computer chose ${computer}. You Tie!`;
+  resultsText.textContent = `The computer chose ${computer}. You Tie!`;
+  resultsScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
 }
 
 function comparePlayers(player, computer) {
   switch (player) {
     case 'Rock':
       if (computer === 'Scissors') {
+        playerScore += 1;
         printWinText(player, computer);
         break;
       } else if (computer === 'Paper') {
+        computerScore += 1;
         printLossText(player, computer);
         break;
       } else if (computer === 'Rock') {
@@ -38,9 +46,11 @@ function comparePlayers(player, computer) {
       }
     case 'Paper':
       if (computer === 'Rock') {
+        playerScore += 1;
         printWinText(player, computer);
         break;
       } else if (computer === 'Scissors') {
+        computerScore += 1;
         printLossText(player, computer);
         break;
       } else if (computer === 'Paper') {
@@ -49,9 +59,11 @@ function comparePlayers(player, computer) {
       }
     case 'Scissors':
       if (computer === 'Paper') {
+        playerScore += 1;
         printWinText(player, computer);
         break;
       } else if (computer === 'Rock') {
+        computerScore += 1;
         printLossText(player, computer);
         break;
       } else if (computer === 'Scissors') {
@@ -63,6 +75,17 @@ function comparePlayers(player, computer) {
 
 function playGame(player, computer) {
   comparePlayers(player, computer);
+  if (playerScore === 5) {
+    alert('You won 5 rounds! Congratulations!');
+    playerScore = 0;
+    computerScore = 0;
+    resultsScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+  } else if (computerScore === 5) {
+    alert('The computer won 5 rounds! Sorry but you lost!');
+    playerScore = 0;
+    computerScore = 0;
+    resultsScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+  }
 }
 
 buttonRock.addEventListener('click', () => {
